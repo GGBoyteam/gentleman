2951e4e6023e0d63d2e7fd1dc15dbd353febd6b5
GGboya <630591905@qq.com>
2023-07-13 11:43:28 +0800

    JSON 解析和验证器封装

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -10,13 +10,11 @@ type SignupEmailExistRequest struct {
 	Email string `json:"email,omitempty" valid:"email"`
 }
 
-func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
-
+func SignupEmailExist(data interface{}, c *gin.Context) map[string][]string {
 	// 自定义验证规则
 	rules := govalidator.MapData{
 		"email": []string{"required", "min:4", "max:30", "email"},
 	}
-
 	// 自定义验证出错时的提示
 	messages := govalidator.MapData{
 		"email": []string{
@@ -26,15 +24,5 @@ func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]str
 			"email:Email 格式不正确，请提供有效的邮箱地址",
 		},
 	}
-
-	// 配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-		Messages:      messages,
-	}
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data, rules, messages)
 }
